4262c56a24ce8905471cb64f801a2ec3424eb1f0
bheimbu <bheimbu@gwdg.de>
2025-11-04 13:09:36 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,11 +22,11 @@ extensions = [
     'sphinxcontrib.images'
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
+#intersphinx_mapping = {
+#    'python': ('https://docs.python.org/3/', None),
+#    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+#}
+#intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
